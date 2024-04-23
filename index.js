d4632fc7df532c80b98bec7b9711e29fa0034be3
ApachiPro <142674667+ApachiPro@users.noreply.github.com>
2024-04-23 16:55:43 -0500

    Updated formatting

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ element.innerHTML = `<div id="bg" style="background-color:black">
         Enable Notifications
     </button>
 </div>
+<br>
 <font size="5" style="color:green"> Apachi Chat Room </font>
 
 <p1 style="color:green">
@@ -94,7 +95,7 @@ element.innerHTML = `<div id="bg" style="background-color:black">
 <br>
 <br>
 
-<div style="background-color:rgb(30,30,30);height:90%" id="frame"> <center><b style="color:lime"id="ping">ping: CONNECTING TO SERVER...</b> <p1 style="color:white"><b>APACHI CHAT ROOM </b><p1 style="color:lime" id="connections"> <b> online users: CONNECTING TO SERVER...</b></p1></p1><br><br> <p1 style="color:white" id="stream"> CONNECTING TO SERVER... *may take a while </p1>
+<div style="background-color:rgb(30,30,30);height:85%;bottom:0.5%;position:absolute" id="frame"> <center><b style="color:lime"id="ping">ping: CONNECTING TO SERVER...</b> <p1 style="color:white"><b>APACHI CHAT ROOM </b><p1 style="color:lime" id="connections"> <b> online users: CONNECTING TO SERVER...</b></p1></p1><br><br> <p1 style="color:white" id="stream"> CONNECTING TO SERVER... *may take a while </p1>
 <br>
 
 
