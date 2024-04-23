7a2a408dac4daa24fc994c217face2c6598d61fe
ApachiPro <142674667+ApachiPro@users.noreply.github.com>
2024-04-22 19:48:50 -0500

    Updated

--- index.js
+++ index.js
@@ -56,12 +56,12 @@ element.innerHTML = `<div id="bg" style="background-color:black">
       <button id="name_changer" style="background-color:black;border-color:green;color:green;width:110px;height:30px;top:6%;left:92.1%;position:absolute">
         Change Name
       </button> 
+      <button id="ssd" style="background-color:black;border-color:green;color:green;width:110px;height:30px;top:8%;left:92.1%;position:absolute">
+        return
+      </button>
     </div>
 	 <font size="5" style="color:green"> Apachi Chat room </font>
    <br>
-   <button id="ssd" style="background-color:black;border-color:green;color:green">
-    return
-   </button>
 	 <button id="sd" style="background-color:black;border-color:green;color:green">
     enable notifications
    </button>
