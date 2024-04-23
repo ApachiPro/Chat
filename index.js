2bf465d7e15107214d8b6e24ca528c436adfd101
ApachiPro <142674667+ApachiPro@users.noreply.github.com>
2024-04-23 17:19:57 -0500

    Updated formatting

--- index.js
+++ index.js
@@ -82,10 +82,14 @@ element.innerHTML = `<div id="bg" style="background-color:black">
         Terms of service
     </button>
     before using the chat room.
+    </p1>
+    <p1>
     <b>
     <br>
         PLEASE USE RESPONSIBLY
     </b>
+    </p1>
+    <p1>
     <br>
     <b>
     Messages reset every seventeen messages.
