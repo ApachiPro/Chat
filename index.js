e113bcaebb7459314301a40bb32c86735e95752e
ApachiPro <142674667+ApachiPro@users.noreply.github.com>
2024-04-23 20:01:24 -0500

    Changed color

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ xh2r.onload = function() {
 
 
 var element = document.createElement("div");
-element.innerHTML = `<div id="bg" style="background-color:black">
+element.innerHTML = `<div id="bg" style="background-color:rgb(0, 48, 82)">
 <title> Apachi Chatroom </title>
 <center>
 <img style="top:2%;left:2%;position:absolute" src="https://raw.githack.com/apachipro/chat/main/Resources/logo.png" />
