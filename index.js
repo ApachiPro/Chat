2bda1e9e52de6dbde9c2154747f773d8ca338515
apachipro32 <167573010+apachipro32@users.noreply.github.com>
2024-04-26 15:20:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var system_status = {
     "stream": "offline",
     "numb": 0
   },
-  version: 'Alpha, v1.0.0 -Hot fix'
+  version: 'Alpha, v1.2.0 -Mega Fix'
 }
 // chat room msg alerts
 var enable_notif = true
